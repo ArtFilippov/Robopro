7c0de1d1f44879857666faa09d2a4b0fdd69c94b
ArtFilippov <artfilipp0w@ya.ru>
2024-09-05 12:24:46 +0300

    wrote submatrix in matrix.h

--- matrix.c
+++ matrix.c
@@ -4,8 +4,16 @@ struct Matrix to_matrix(double *matrix, int n)
 {
     struct Matrix new_matrix = {.n = n};
     for (int i = 0; i < n; ++i) {
-        new_matrix.m[i] = matrix + i * n;
+        new_matrix.data[i] = matrix + i * n;
     }
 
     return new_matrix;
+}
+
+struct Matrix submatrix(struct Matrix matrix, int *rows, int n)
+{
+    struct Matrix new_matrix = {.n = n};
+    for (int i = 0; i < n; ++i) {
+        new_matrix.data[i] = matrix.data[rows[i]] + matrix.n - n;
+    }
 }
