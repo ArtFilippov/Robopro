31b6cb8350fcc175f573ed3976fd9d68325285c5
ArtFilippov <artfilipp0w@ya.ru>
2024-09-05 12:17:39 +0300

    fix submatrix() in  matrix.h

--- matrix.c
+++ matrix.c
@@ -2,5 +2,10 @@
 
 struct Matrix to_matrix(double *matrix, int n)
 {
-    
+    struct Matrix new_matrix = {.n = n};
+    for (int i = 0; i < n; ++i) {
+        new_matrix.m[i] = matrix + i * n;
+    }
+
+    return new_matrix;
 }
--- matrix.h
+++ matrix.h
@@ -8,4 +8,4 @@ struct Matrix to_matrix(double *matrix, int n);
 
 double det(struct Matrix matrix);
 
-struct Matrix submatrix(struct Matrix matrix, int *rows, int *columns, int n);
+struct Matrix submatrix(struct Matrix matrix, int *rows, int n);
