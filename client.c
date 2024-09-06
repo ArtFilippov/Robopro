19d2b0d9732830289edc926fb01732a28ea427f0
ArtFilippov <artfilipp0w@ya.ru>
2024-09-06 07:19:04 +0300

    fix get_matrix

--- client.c
+++ client.c
@@ -8,15 +8,18 @@
 void get_matrix(double *matrix, int n)
 {
     srand(time(NULL));
-    for (int i = 0; i < n * n; ++i) {
-        matrix[i] = -MAX_INT_PART + rand() % MAX_INT_PART + (double) rand() / RAND_MAX;
+    for (int i = 0; i < n * n; ++i)
+    {
+        matrix[i] = -MAX_INT_PART + 2 * (rand() % MAX_INT_PART) + (double)rand() / RAND_MAX;
     }
 }
 
 void show_matrix(double *matrix, int n)
 {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
             printf("%lf ", matrix[i * n + j]);
         }
         printf("\n");
@@ -25,7 +28,8 @@ void show_matrix(double *matrix, int n)
 
 void run_by_key(int sock)
 {
-    while (getchar() == '\n') {
+    while (getchar() == '\n')
+    {
         double matrix[N * N];
         get_matrix(matrix, N);
 
@@ -38,7 +42,8 @@ void run_by_key(int sock)
         }
 
         char r_buf[MAX_RESPONSE];
-        if (recvall(sock, r_buf, MAX_RESPONSE, 0) <= 0) {
+        if (recvall(sock, r_buf, MAX_RESPONSE, 0) <= 0)
+        {
             fprintf(stderr, "bed recv\n");
             exit(1);
         }
