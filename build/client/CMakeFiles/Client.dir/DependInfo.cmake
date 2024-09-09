70efbe02148f13155ea14ef84e9100b46a73bdf9
ArtFilippov <artfilipp0w@yandex.ru>
2024-09-09 16:32:42 +0300

    restructured the directories

---
+++ build/client/CMakeFiles/Client.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/Robopro/src/client/client.c" "client/CMakeFiles/Client.dir/client.c.o" "gcc" "client/CMakeFiles/Client.dir/client.c.o.d"
+  "/home/user/Robopro/src/client/client_main.c" "client/CMakeFiles/Client.dir/client_main.c.o" "gcc" "client/CMakeFiles/Client.dir/client_main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/common/CMakeFiles/SendRecv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SendRecv.dir/send_recv.c.o"
+  "CMakeFiles/SendRecv.dir/send_recv.c.o.d"
+  "libSendRecv.a"
+  "libSendRecv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SendRecv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
