01ec765beb18c5d58ad5dda24c88909c34f7ae99
ArtFilippov <artfilipp0w@yandex.ru>
2024-09-09 17:19:54 +0300

    fix errors

--- src/client/client.h
+++ src/client/client.h
@@ -1,9 +1,12 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-const int N = 6;
-const int MATRIX_SIZE = sizeof(double) * N * N;
-const int MAX_INT_PART = 10;
+enum
+{
+    N = 6,
+    MATRIX_SIZE = sizeof(double) * N * N,
+    MAX_INT_PART = 10
+};
 
 void run(int sock);
 void get_matrix(double *matrix, int n);
